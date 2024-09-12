ebceffc4be3766542330cac219346ec324c12a3c
PedroMunizCarvalho <pcatmuniz@gmail.com>
2024-09-12 15:56:42 -0300

    Upload de aplicação de pesquisa de atletas

---
+++ dados.js
@@ -0,0 +1,23 @@
+let dados = [
+    {
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta Brasileira que conquistou, ouro e prata no Mundial de 2021 em Kitakyushu; ouro e bronze em Liverpool 2022; e ouro, prata e bronze na Antuérpia 2023. Rebeca também conquistou dois ouros nos Jogos Pan-Americanos de Santiago 2023. Vice-campeã olímpica do salto em 2024 e bronze na competição por equipes.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica rebeca andrade ginastica ouro"
+    },
+
+    {
+        titulo: "Isaquias Queiroz",
+        descricao: "Isaquias é considerado um dos maiores atletas olímpicos da história do Brasil, destacando-se por seu talento, determinação e conquistas em competições internacionais. Reconhecido mundialmente por suas habilidades e resistência na canoagem, Isaquias tornou-se uma referência na canoagem. Seu desempenho foi motivo de orgulho nacional e o consolidou como uma das maiores estrelas do esporte brasileiro",
+        link: "https://pt.wikipedia.org/wiki/Isaquias_Queiroz",
+        tags: "canoagem água isaquias queiroz prata "
+    },
+
+    {
+        titulo: "Gabriel Medina",
+        descricao: "Gabriel Medina é um dos maiores nomes da história do surf mundial e uma verdadeira referência do esporte no Brasil. Medina fez história ao ser o primeiro brasileiro a conquistar o título mundial de surf, tornando-se campeão do World Surf League (WSL) em 2014. Sua vitória foi um marco, consolidando o Brasil como uma potência no cenário do surf mundial. Ele repetiu o feito em 2018, conquistando seu segundo título mundial, e em 2021, tornando-se tricampeão mundial, um feito inédito para um atleta brasileiro.",
+        link: "https://pt.wikipedia.org/wiki/Gabriel_Medina",
+        tags: "mediana gabriel surf surfe mar água bronze melhor foto tirada nas olimpíadas ",
+    }
+    
+]
